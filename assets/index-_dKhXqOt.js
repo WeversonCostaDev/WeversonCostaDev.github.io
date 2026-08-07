1696835ac117b168a229cfa1d488c65131838ecb
PC-Trabalho <trabalho@gmail.com>
2026-08-07 16:10:02 -0300

    nova versão com rodapé

---
+++ assets/index-_dKhXqOt.js
@@ -0,0 +1,174 @@
+(function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),t.credentials=e.crossOrigin===`use-credentials`?`include`:e.crossOrigin===`anonymous`?`omit`:`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();var e=`/assets/rioJaguaribe-Z3DOBLRg.jpg`,t=`/assets/ifce-BWMG-GDl.png`,n=`/assets/castanhao-BBuJMvy_.jpg`,r=`/assets/pontal-maceio-DS6AVecd.jpg`,i=`/assets/mirante-barra-DE9KhckF.jpg`,a=`/assets/aracati-teatro-BmN3Fd0b.jpeg`,o=`/assets/canoa-quebrada-broadway-B7R_kLNl.jpg`,s=`/assets/simbolo-canoa-quebrada-BXt7z9EH.jpg`,c=`/assets/rua-grande-Bj6y8SXQ.jpg`,l=`/assets/plato-lagoa-do-mato-DKCy8m71.jpeg`,u=`/assets/igreja-matriz-Y3WgYV-E.jpg`;document.querySelector(`#app`).innerHTML=`
+      <header>
+        <div id= "faixa-superior">
+          <nav >
+            <a class= "link-unico estilo-texto" href= "https://portal.ifce.edu.br/campus/aracati/">Instituto Federal de Educação, Ciência e Tecnologia do Ceará</a>
+          </nav>
+            <nav class="menu">
+              <a href="#ouvidoria"><i class="fa-solid fa-headset"></i>Ouvidoria</a>
+              <a href="#transparencia"><i class="fa-solid fa-circle-info"></i>Transparência</a>
+              <a href="#Acessibilidade"><i class="fa-solid fa-face-smile"></i>Acessibilidade</a>
+              <a href="#pesquisar"><i class="fa-solid fa-magnifying-glass"></i>Pesquisar</a>
+
+              <a href="#facebook"><i class="fa-brands fa-square-facebook icon"></i></a>
+              <a href="#instagram"><i class="fa-brands fa-square-instagram icon"></i></i></a>
+              <a href="#email"><i class="fa-solid fa-envelope icon"></i></i></a>
+            </nav>
+        </div>
+        <div id="faixa-meio">
+          <img id="logo" src="${t}">
+        </div>
+        <div id="faixa-inferior">
+          <nav class= "menu">
+            <a href="index.html">Página inicial</a>
+            <a href="about.html">Sobre o site</a>
+            <a href="contact.html">Página de contato</a>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+        <section class= "conteudo-main">
+          <div class=galeria>
+            <img class="imagem" src= "${e}">
+            <details class= "detalhes-galeria">
+              <summary>
+                Rio do Jaguaribe
+              </summary>
+                <p>
+                  O Rio Jaguaribe é o maior e mais importante rio do estado do Ceará, desempenhando um papel fundamental para o desenvolvimento econômico, social e ambiental da região. Com aproximadamente 610 quilômetros de extensão, ele atravessa diversos municípios do Vale do Jaguaribe, abastecendo cidades, irrigando áreas agrícolas e contribuindo para atividades como a pesca e o turismo. Além de sua importância para o abastecimento de água, o rio oferece belas paisagens naturais e é um dos principais símbolos da identidade e da história do Vale do Jaguaribe.
+                </p>
+                <iframe class="inframe-galeria" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d930122.5848697021!2d-38.76388878880472!3d-4.959138292672579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b84da03cf58113%3A0xcc015cac9d522075!2sRio%20Jaguaribe!5e1!3m2!1spt-BR!2sbr!4v1785774852063!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+            </details>
+
+            <img class="imagem" src= "${r}">
+            <details class= detalhes-galeria>
+              <summary>
+                Pontal de Maceió, Fortim-CE
+              </summary>
+                <p>
+                  O Pontal do Maceió, localizado no município de Fortim, é um dos destinos mais encantadores do litoral leste cearense. A praia se destaca pelas águas calmas, areia clara e clima tranquilo, sendo ideal para quem busca descanso e contato com a natureza. Além da beleza natural, o local é conhecido pelo encontro do Rio Jaguaribe com o mar, pelos passeios de barco, esportes como o kitesurf e pelos belos pores do sol, tornando-se uma das principais atrações turísticas do Vale do Jaguaribe.
+                </p>
+                <iframe class= "inframe-galeria"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.050191439734!2d-37.78564512545792!3d-4.401711595572416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b84f8e7e6e1b3b%3A0xca694e89adf003f7!2sPraia%20de%20Pontal%20de%20Macei%C3%B3!5e0!3m2!1spt-BR!2sbr!4v1785853638930!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+            </details>
+
+            <img class="imagem" src= "${n}">
+            <details class="detalhes-galeria">
+              <summary>
+                Castanhão, Alto Santo-CE
+                </summary>
+                <p>
+                  O Açude Castanhão, localizado entre os municípios de Alto Santo, Jaguaribara e Jaguaribe, é o maior reservatório de água do Ceará e um dos maiores do Brasil. Construído sobre o Rio Jaguaribe, ele desempenha um papel essencial no abastecimento de água, na irrigação, na piscicultura e no controle das secas no estado. Além de sua importância econômica e ambiental, o Castanhão também atrai visitantes por suas belas paisagens, passeios de barco e pela história da antiga cidade de Jaguaribara, que foi transferida devido à formação do reservatório.
+                </p>
+                  <iframe class="inframe-galeria" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29042.827753496873!2d-38.44606241305673!3d-5.4671247431375605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7bb999ef173c059%3A0xe85a4b966c2aae09!2sCastanh%C3%A3o%2C%20Alto%20Santo%20-%20CE%2C%2062970-000!5e1!3m2!1spt-BR!2sbr!4v1785777429206!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+              </details>
+
+            <img class="imagem" src= "${i}">
+            <details class=detalhes-galeria>
+              <summary>
+                Mirante da barra, Fortim-CE
+              </summary>
+                <p>
+                  O Mirante da Barra, localizado no município de Fortim, é um dos melhores lugares para apreciar a beleza natural do Vale do Jaguaribe. Do alto do mirante é possível observar o encontro do Rio Jaguaribe com o Oceano Atlântico, formando uma paisagem marcada por manguezais, dunas e praias. O local é bastante procurado por visitantes para contemplar o pôr do sol, tirar fotografias e apreciar a tranquilidade da região.
+                </p>
+                <iframe class="inframe-galeria" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.7565252691643!2d-37.79738532545756!3d-4.456321495517952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b84e111c36bf13%3A0x79e023918ed8334d!2sMirante%20da%20Barra%20de%20Fortim!5e0!3m2!1spt-BR!2sbr!4v1785853711969!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+            </details>
+
+            <img class="imagem" src= "${a}">
+              <details class=detalhes-galeria>
+                <summary>
+                  Teatro Francisca Clotilde, Aracati-CE
+                </summary>
+                  <p>
+                    O Teatro Francisca Clotilde, localizado no Centro Histórico de Aracati, é um importante patrimônio cultural do Vale do Jaguaribe. Inaugurado no século XIX e restaurado para preservar sua arquitetura original, o teatro recebe apresentações de música, dança, teatro e diversos eventos culturais ao longo do ano. Seu nome homenageia a escritora e educadora cearense Francisca Clotilde, tornando o espaço um símbolo da história, da arte e da cultura da região.
+                  </p>
+                  <iframe class="inframe-galeria"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.1632475633796!2d-37.77518072545687!3d-4.56465809540988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b9b2f95cf601ed%3A0x9b9e966d16957e39!2sTeatro%20Francisca%20Clotilde!5e0!3m2!1spt-BR!2sbr!4v1785853786064!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+              </details>
+
+              <img class="imagem" src= "${l}">
+              <details class=detalhes-galeria>
+                <summary>
+                  Platô, Lagoa do mato, Aracati-CE  
+                </summary>
+                  <p>
+                    O Platô de Lagoa do Mato, localizado no distrito de Lagoa do Mato, em Aracati, é um dos mirantes naturais mais impressionantes do litoral leste cearense. Do alto do platô é possível contemplar uma vista panorâmica das falésias, dunas, lagoas e da vegetação característica da região. O local é bastante procurado por visitantes para apreciar o nascer e o pôr do sol, tirar fotografias e desfrutar da tranquilidade e da beleza natural do Vale do Jaguaribe.
+                  </p>
+                  <iframe class="inframe-galeria" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.0134195573655!2d-37.63661512545659!3d-4.591614295383008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b9cbbd432d9245%3A0xcf7571d33c723050!2sPlat%C3%B4%20de%20Lagoa%20do%20Mato!5e0!3m2!1spt-BR!2sbr!4v1785853869029!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+              </details>
+
+              <img class="imagem" src= "${c}">
+              <details class=detalhes-galeria>
+                <summary>
+                  Casas Históricas, Aracati-CE  
+                </summary>
+                  <p>
+                    A Rua Grande, oficialmente chamada Rua Coronel Alexanzito, é uma das vias mais tradicionais e movimentadas de Aracati. Localizada no Centro Histórico da cidade, ela reúne casarões coloniais preservados, lojas, restaurantes e importantes edifícios históricos que retratam a riqueza cultural e arquitetônica do município. Caminhar pela Rua Grande é uma oportunidade de conhecer a história de Aracati e apreciar um dos conjuntos urbanos mais bem preservados do Ceará.
+                  </p>
+                  <iframe class="inframe-galeria" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127269.03226503175!2d-37.91335948359373!3d-4.565738699999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b9b2f9400f5e5f%3A0x28330a307fb5512f!2sCasas%20Historicas!5e0!3m2!1spt-BR!2sbr!4v1785853956540!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+              </details>
+
+              <img class="imagem" src= "${u}">
+              <details class=detalhes-galeria>
+                <summary>
+                  Igreja Matriz de Nossa Senhora do Rosário, Aracati-CE  
+                </summary>
+                  <p>
+                    A Igreja Matriz de Nossa Senhora do Rosário, localizada no Centro Histórico de Aracati, é um dos mais importantes patrimônios religiosos e arquitetônicos do Vale do Jaguaribe. Construída no século XVIII, a igreja destaca-se por sua arquitetura colonial e por sua relevância histórica para a formação da cidade. Além de ser um importante local de fé, o templo atrai visitantes interessados em conhecer a história, a cultura e a beleza do patrimônio arquitetônico de Aracati.
+                  </p>
+                  <iframe class="inframe-galeria" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127269.03393309479!2d-37.91335976382632!3d-4.565729296192161!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b9b2fecc33f669%3A0x83f731f4eea776de!2sIgreja%20Matriz%20Nossa%20Senhora%20do%20Ros%C3%A1rio!5e0!3m2!1spt-BR!2sbr!4v1785854034392!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+              </details>
+
+              <img class="imagem" src= "${o}">
+              <details class=detalhes-galeria>
+                <summary>
+                  Broadway, Canoa-Quebrada, Aracati-CE
+                </summary>
+                  <p>
+                    A Broadway é a principal rua de Canoa Quebrada, em Aracati, e um dos locais mais visitados do litoral leste cearense. Conhecida por seu ambiente descontraído e vibrante, a via reúne restaurantes, bares, lojas de artesanato, cafeterias e espaços de lazer que atraem turistas durante todo o ano. À noite, a Broadway ganha ainda mais movimento, tornando-se o principal ponto de encontro de moradores e visitantes que desejam aproveitar a gastronomia, a cultura e a vida noturna da região.
+                  </p>
+                  <iframe class="inframe-galeria" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.3766162870643!2d-37.70593852545719!3d-4.52599349544843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b84b3f60e123cf%3A0x1f5cef5c6f6162cc!2sR.%20Drag%C3%A3o%20do%20Mar%20-%20Canoa%20Quebrada%2C%20Aracati%20-%20CE%2C%2062800-000!5e0!3m2!1spt-BR!2sbr!4v1785854091988!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+              </details>
+
+              <img class="imagem" src= "${s}">
+              <details class=detalhes-galeria>
+                <summary>
+                  Simbolo lua e estrela, Canoa-Quebrada, Aracati-CE
+                </summary>
+                  <p>
+                    O símbolo da Lua e Estrela é um dos maiores ícones de Canoa Quebrada, em Aracati, e representa a identidade da praia em todo o Brasil. Esculpido nas falésias de areia avermelhada, o desenho tornou-se um cartão-postal da região e um dos locais mais fotografados pelos visitantes. Embora existam diferentes histórias sobre sua origem, a lua e a estrela simbolizam o espírito de liberdade, beleza e encanto que fizeram de Canoa Quebrada um dos destinos turísticos mais famosos do Ceará e do Vale do Jaguaribe.
+                  </p>
+                  <iframe class="inframe-galeria" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.377469625514!2d-37.71002096511227!3d-4.5258382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b84b3bdd6b453d%3A0x7a27d98d8f3237ee!2sEstrela%20de%20Canoa%20Quebrada%20nas%20Dunas!5e0!3m2!1spt-BR!2sbr!4v1785854169457!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+              </details>
+          </div>
+
+          <div class="videos"> 
+            <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/5UYcWR0bD3c?si=mBHSmYci5_dr4cdA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/DenFOkDn1Hk?si=pGjCG_coKsYPWyqC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            
+            <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/BHT18Htm7n4?si=VskZEZ_ddO4yrQXz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            
+            <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/CCat4Gr1GmE?si=XPd6kT6FubqsIXpb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            
+            <h2 class="estilo-texto">Baixe aqui nosso PDF sobre turismo</h2>
+            <div class=card>
+              <a class="link-unico estilo-texto-2" href='./assets/turismo.pdf' download="turismo">
+                Baixar pdf
+              </a>
+            </div>
+          </div>
+
+        </section>
+      </main>
+      <footer id="rodape">
+        <p class="estilo-texto">© 2026 - Desenvolvido por Weverson Costa</p>
+        <p class="estilo-texto">Ciência da Computação - IFCE Campus Aracati</p>
+        
+        <nav>
+          <a href="#email">Contato</a>
+          <a href="#instagram">Instagram</a>
+          <a href="#github">GitHub</a>
+        </nav>
+      </footer>
+
+  `;
